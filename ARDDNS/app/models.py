318f6f2ecd784627a15cf484b81deb7d0d78e6ed
kike <enrique.goberna@udc.es>
2017-03-31 22:32:40 +0200

    Añadidas funciones 'get_api_key'y 'to_json' a la clase Device

--- ARDDNS/app/models.py
+++ ARDDNS/app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+import json
+
+from app.util import build_api_key
+
 # Create your models here.
 
 class Device(models.Model):
@@ -9,4 +13,45 @@ class Device(models.Model):
 	ip = models.CharField(max_length = 50, null = True, unique = True)
 	last_seen = models.DateTimeField(null = True)
 	location = models.CharField(max_length = 100, null = True)
+
+	def get_api_key(self):
+
+		####################################################################
+
+		# Función que devuelve el api key del dispositivo, delegando en la 
+		# función 'build_api_key' del módulo app.util
+
+		# Entradas:
+
+		# Salidas:
+		#	- Información del dispositivo en formato JSON.
+
+		####################################################################
+
+
+
+		return build_api_key(self.hostname, self.mac_address)
+
+	def to_json(self):
+
+		####################################################################
+
+		# Función que convierte la información del dispositivo a formato
+		# JSON.
+
+		# Entradas:
+
+		# Salidas:
+		#	- Información del dispositivo en formato JSON.
+
+		####################################################################
+
+		data = {}
+		data["mac_address"] = self.mac_address
+		data["hostname"] = self.hostname
+		data["ip"] = self.ip
+		# data["last_seen"] = self.last_seen
+		data["location"] = self.location
+
+		return json.dumps(data)
 
